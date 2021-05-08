bf40d1c9be063e0feca8079c1ad22a6a46c239ed
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2021-05-08 08:31:46 +0700

    Add options for merging

--- src/store/merge-action.js
+++ src/store/merge-action.js
@@ -8,3 +8,21 @@ export default (...listApis) => {
   });
   return actions;
 };
+
+/**
+ * @param {any} api
+ * @param {{ include: string[], exclude: string[], prefix: string }} opts
+ * @returns
+ */
+export const mergeActionsWithOptions = (api, opts = {}) => {
+  const { include = null, exclude = [], prefix = "" } = opts;
+  const actions = {};
+  let includedActions = include ?? Object.keys(api);
+  includedActions = includedActions.filter(
+    action => action.startsWith(prefix) && exclude.indexOf(action) === -1
+  );
+  includedActions.forEach(actionName => {
+    actions[actionName] = (context, data) => api?.(data);
+  });
+  return actions;
+};
