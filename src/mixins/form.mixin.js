639aefa4c8da3a8c40ec6b95398f769880b5c926
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2021-05-29 10:28:49 +0700

    Add form mixin

---
+++ src/mixins/form.mixin.js
@@ -0,0 +1,33 @@
+export default validatorField => ({
+  props: {
+    data: {
+      type: Object
+    }
+  },
+  data: () => ({
+    form: {}
+  }),
+  computed: {
+    validator() {
+      return this.$refs[validatorField];
+    },
+    isDataChanged() {
+      for (let field in this.form) {
+        if (this.data[field] !== this.form[field]) {
+          return true;
+        }
+      }
+      return false;
+    }
+  },
+  methods: {
+    async $on_submittedForm() {
+      const isFormValid = await this.validator.validate();
+      if (!isFormValid) {
+        return;
+      }
+      this.validator.reset();
+      this.$emit("submitted", { validator: this.validator, form: this.form });
+    }
+  }
+});
--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,2 +1,3 @@
 export { default as TestMixin } from "./test.mixin";
 export { default as TestResultMixin } from "./test-result.mixin";
+export { default as FormMixin } from "./form.mixin";
