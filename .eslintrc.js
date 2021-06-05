4fa562e5014452901b7c726bbbb8ae04ca0d9a67
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2021-06-05 21:20:18 +0700

    Ignore component binding

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
     parser: "babel-eslint"
   },
   rules: {
+    "vue/no-unused-components": ["off", {
+      "ignoreWhenBindingPresent": false
+    }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": process.env.NODE_ENV === "production" ? "warn" : "off"
