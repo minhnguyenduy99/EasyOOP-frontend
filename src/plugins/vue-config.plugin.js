4f45175c4bd09371bbced1ee1d0ba6d908af24eb
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2021-07-03 21:22:21 +0700

    - Update to use environment variables - Add facebook plugin

--- src/plugins/vue-config.plugin.js
+++ src/plugins/vue-config.plugin.js
@@ -1,10 +1,7 @@
-import config from "../../config/app-config.json";
-
 export default {
   install(Vue, options = {}) {
     const { key = "$appConfig" } = options;
-    const env = process.env.NODE_ENV || "development";
-    const appConfig = config[env];
+    const appConfig = process.env;
     Vue.prototype[key] = appConfig;
   }
 };
